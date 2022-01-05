13757acb053c4f37e32763586b703acd4267908b
Bluette1 <marylene.sawyer@gmail.com>
2022-01-05 16:54:07 +0200

    Add cross and circle

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-web";
 import bg from "./assets/bg.jpeg";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
-        <StatusBar style="auto" />
-        <View style={styles.circle}>
-          <View style={styles.innerCircle}></View>
-        </View>
-        <View>
-          <View styles={styles.crossLine}></View>
-          <View styles={styles.crossLine}></View>
+        <View style={styles.map}>
+          <View style={styles.circle}></View>
+          <View style={styles.cross}>
+            <View style={styles.crossLine}></View>
+            <View style={[styles.crossLine, styles.crossLineReversed]}></View>
+          </View>
         </View>
       </ImageBackground>
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -34,23 +36,35 @@ const styles = StyleSheet.create({
     paddingTop: 15,
   },
   circle: {
-    height: 75,
-    width: 75,
-    backgroundColor: "white",
+    position: "absolute",
+    left: 1 * 87,
+    top: 2 * 92,
+    height: 73,
+    width: 73,
+    borderWidth: 10,
+    borderColor: "white",
     borderRadius: 50,
     alignItems: "center",
     justifyContent: "center",
     margin: 10,
   },
-  innerCircle: {
-    height: 50,
-    width: 50,
-    backgroundColor: "#242D34",
-    borderRadius: 50,
+  cross: { width: 75, height: 75},
+  map: {
+    borderWidth: 10,
+    borderColor: "white",
+    width: "80%",
+    aspectRatio: 1,
   },
   crossLine: {
+    position: "absolute",
+    left: 32.5,
+    width: 10,
+    height: 70,
+    borderRadius: 5,
     backgroundColor: "white",
-    height: 50,
-    width: 10
-  }
+    transform: [{ rotate: "45deg" }],
+  },
+  crossLineReversed: {
+    transform: [{ rotate: "-45deg" }],
+  },
 });
